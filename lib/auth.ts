d44f43c242d71403d186a3b1fc781889111a770d
Aymen Sakouhi <aymenmarketer@gmail.com>
2025-11-20 23:47:13 +0100

    add better auth

---
+++ lib/auth.ts
@@ -0,0 +1,9 @@
+import { betterAuth } from 'better-auth'
+import { prismaAdapter } from 'better-auth/adapters/prisma'
+import { Prisma } from './generated/prisma/client'
+
+export const auth = betterAuth({
+  database: prismaAdapter(Prisma, {
+    provider: 'postgresql',
+  }),
+})
